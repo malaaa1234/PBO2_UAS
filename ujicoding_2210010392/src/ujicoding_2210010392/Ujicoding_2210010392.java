2fc1bc765cbdb7d453f8f6890d7e8336c42b79c9
malaaa1234 <ns354446@gmail.com>
2025-01-16 14:02:51 +0700

    Add files via upload

---
+++ ujicoding_2210010392/src/ujicoding_2210010392/Ujicoding_2210010392.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ujicoding_2210010392;
+import javax.swing.JOptionPane;
+import gui.Framenilai;
+import sys.crudtable;
+
+/**
+ *
+ * @author Acer
+ */
+public class Ujicoding_2210010392 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Framenilai().setVisible(true);
+    }
+    
+}
